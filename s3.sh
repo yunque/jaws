e4380ba9b7a50d2b94f1e99712ac5126a19502b7
yunque <yunque@users.noreply.github.com>
2017-02-11 23:38:26 +0000

    Delete and sync files between local dir and bucket
    Can either delete a file from the local dir first and then sync the bucket,
    or delete an object from the bucket and then sync the local dir.

--- s3.sh
+++ s3.sh
@@ -13,3 +13,17 @@ aws s3 ls
 aws s3 ls s3://bucket-test
 # List objects inside a folder
 aws s3 ls s3://bucket-test/subdir
+
+# # Delete a local file and sync the bucket
+# Delete local file
+rm ./file.txt
+# Attempt sync without --delete option - nothing happens
+aws s3 sync . s3://bucket-test/path
+# Sync with deletion - object is deleted from bucket
+aws s3 sync . s3://my-bucket/path --delete
+
+# # Delete an object from bucket and sync local directory
+# Delete object from bucket
+aws s3 rm s3://bucket-test/path/file.txt
+# Sync with deletion - local file is deleted
+aws s3 sync s3://bucket-test/path . --delete
