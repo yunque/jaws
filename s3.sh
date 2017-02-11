2d4c44c695f300b9264bbfd6181d8761b7a13ece
yunque <yunque@users.noreply.github.com>
2017-02-11 23:30:34 +0000

    List buckets and their contents
    Also added caveat for previous bucket removal command.

--- s3.sh
+++ s3.sh
@@ -2,6 +2,14 @@
 
 # Create bucket
 aws s3 mb s3://bucket-test
-
-# Remove bucket
+# Remove bucket (must be empty)
 aws s3 rm s3://bucket-test
+# Remove non-empty bucket
+aws s3 rm s3://bucket-test --force
+
+# List buckets
+aws s3 ls
+# List objects and folders in a bucket
+aws s3 ls s3://bucket-test
+# List objects inside a folder
+aws s3 ls s3://bucket-test/subdir
